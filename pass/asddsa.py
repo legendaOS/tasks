d717402140a00fb3670fc42a82de4b868db4b06e
legendaOS <76245715+legendaOS@users.noreply.github.com>
2021-02-08 19:18:57 +0300

    sdf

---
+++ pass/asddsa.py
@@ -0,0 +1,11 @@
+def dfggfd():
+    print('Получено')
+    return('jhfghj')
+
+def dfghghjd():
+    print('Получено')
+    return('dfgjhgfgh')
+
+def dfgggd():
+    print('Получено')
+    return('jhsdfvbnfghj')
