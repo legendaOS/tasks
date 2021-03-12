cc80ba6f79579697cd88ac34ef911d6f3087725f
legendaOS <robolegenda@gmail.com>
2021-03-12 18:55:51 +0300

    реализованы ходы без проверок подлинности и завершения игры

--- chess/index.py
+++ chess/index.py
@@ -1,3 +1,31 @@
+def find_el(spisok, elem, flag = True, left = 0, right = None):
+    '''
+    find_el - находит индекс(ы) элемента(ов) в списке
+    spisok - список
+    elem - елемент для поиска
+    flag - True(первое вхождение), False(список вхождений)
+    left, right - левая и правая границы поиска
+    '''
+    if flag == False: buf = []
+    index = left
+    if right == None:
+        right = len(spisok) 
+    while index < right:
+        if spisok[index] == elem:
+            if flag:
+                return index
+            else:
+                buf.append(index)
+        index += 1
+    if flag == False and buf != []:
+        return buf
+    return -1
+
+player_color = True
+#True - белый цвет игрока
+#False - черный цвет игрока
+playing = True
+
 pole = [
     [['r', 0],['n', 0],['b', 0],['k', 0],['q', 0],['b', 0],['n', 0],['r', 0]],
     [['p', 0],['p', 0],['p', 0],['p', 0],['p', 0],['p', 0],['p', 0],['p', 0]],
@@ -38,11 +66,40 @@ def render(pole_list, numbers_list, letters_list):
     return True
 
 def move(stroka, pole_list, numbers_list, letters_list):
-    buf = stroka.split('-')
-    print(buf[0])
-    print(buf[1])
+    buf = stroka.split(' ')
+    coord1 = []
+    coord1.append(find_el(numbers_list,buf[0][1]))
+    coord1.append(find_el(letters_list,buf[0][0]))
+    coord2 = []
+    coord2.append(find_el(numbers_list,buf[1][1]))
+    coord2.append(find_el(letters_list,buf[1][0]))
+    super_bufer = pole_list[coord1[0]][coord1[1]].copy()
+    pole_list[coord1[0]][coord1[1]] = []
+    pole_list[coord2[0]][coord2[1]] = super_bufer
 
-move('e2-e4', pole, numbers, letters)
 
-render(pole, numbers, letters)
+#move('e2 e4', pole, numbers, letters)
 
+#render(pole, numbers, letters)
+
+def main(pole = pole, numbers = numbers, letters = letters, player_color = player_color, playing = playing):
+    count = 1
+    while playing:
+        print('Ход:', count)
+        count += 1
+        if player_color:
+            print('Ходит первый игрок')
+        else:
+            print('Ходит второй игрок')
+        render(pole, numbers, letters)
+        print('Введите ход')
+        in_stroka = input()
+        if in_stroka == '':
+            playing = False
+            continue
+        move(in_stroka, pole, numbers, letters)
+        player_color = not player_color
+        
+    return 0
+
+main()
