4939fdebdcf4da22e93434aa0bf989b01add1cca
legendaOS <76245715+legendaOS@users.noreply.github.com>
2021-03-29 20:14:23 +0300

    Сергея и Марка

---
+++ калькуляторы/jjj.py
@@ -0,0 +1,17 @@
+a=input("Введите 1 число:")
+a=int(a)
+b=input("Введите знак:")
+c=input("Введите 2 число:")
+c=int(c)
+if b=="+"
+    print(a+c):
+else:
+    if "*"==b:
+            print(a*c)
+    else:
+        if"/"==b:
+            print(a/c)
+        else:
+            if"-"==b:
+                print(a-c)
+print(a,"+",b,"=",a+b)
