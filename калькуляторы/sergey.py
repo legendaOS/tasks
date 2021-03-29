6000ad1a044c5ae778ea10f4ed9436cd4b01bc53
legendaOS <robolegenda@gmail.com>
2021-03-29 20:11:53 +0300

    тут калькулятор Сергея

---
+++ калькуляторы/sergey.py
@@ -0,0 +1,18 @@
+a=int(input("BBeDNTe nepBoe чNCLo"))
+b=int(input("BBeDNTe nepBoe чNCLo"))
+c=input("BBeDNTe зHaK")
+if c=="+":
+    print(a+b)
+elif c == '-':
+    print(a-b)
+elif c == "*":
+    print(a*b)
+
+elif c=="/" and b==0:
+    print("Windows Error")
+elif c == "/":
+    print(a/b)
+else:
+    print("мур мур асыпка")
+    
+
