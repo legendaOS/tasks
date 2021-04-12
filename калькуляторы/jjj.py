f224aefe79d0cc0b5adff502f5bf273546e0a09e
legendaOS <76245715+legendaOS@users.noreply.github.com>
2021-04-12 20:02:53 +0300

    mjfkjyvckhgc

--- калькуляторы/jjj.py
+++ калькуляторы/jjj.py
@@ -1,17 +1,35 @@
-a=input("Введите 1 число:")
-a=int(a)
-b=input("Введите знак:")
-c=input("Введите 2 число:")
-c=int(c)
-if b=="+"
-    print(a+c):
-else:
-    if "*"==b:
-            print(a*c)
-    else:
-        if"/"==b:
-            print(a/c)
-        else:
-            if"-"==b:
-                print(a-c)
-print(a,"+",b,"=",a+b)
+memory=0
+while True:
+    
+    a=input("Введите 1 число:")
+    
+    if a=="mr":
+        print(memory)
+        continue
+    
+    if a=="mc":
+        memory=0
+        continue
+
+
+    a=int(a)
+    b=input("Введите знак:")
+
+    if b=="m+":
+        memory+=a
+        continue
+
+    c=input("Введите 2 число:")
+    c=int(c)
+    k="Итог:"
+
+    if b=="+":
+        print(k,a+c)
+    elif "*"==b:
+        print(k,a*c)
+    elif "/"==b and b==0:
+        print('ТЫ БОЛЬНОЙ???') 
+    elif "/"==b:
+        print(k,a/c)
+    elif "-"==b:
+        print(k,a-c)
